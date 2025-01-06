9418dddf4d7743ef3944e468155180e403a73c73
Rahidul Islam <rahiseli@outlook.com>
2025-01-06 07:26:50 +0600

    some line is commented

--- practice_packages/main.py
+++ practice_packages/main.py
@@ -2,11 +2,11 @@ from sales.order import create_sales_order
 from sales.billing import create_billing
 from sales.delivery import create_delivery
 from sales import TAX_RATE
-from sales import *
+# from sales import *
 create_sales_order()
 create_billing()
 create_delivery()
 print(TAX_RATE)
-order.create_sales_order()
-billing.create_billing()
-delivery.create_delivery()
+# order.create_sales_order()
+# billing.create_billing()
+# delivery.create_delivery()
