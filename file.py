29dee41dd02d6bced945668bb948fd5137a22735
Rahidul Islam <rahiseli@outlook.com>
2025-01-08 09:31:50 +0600

    i learn file reading

--- file.py
+++ file.py
@@ -1,14 +1,26 @@
-# write file
-f = open("test.txt", "w")
-f.write("Hello World")
-f.close()
+# # write file
+# f = open("test.txt", "w")
+# f.write("Hello World")
+# f.close()
 
-# read file
-f = open("test.txt", "r")
-print(f.read())
-f.close()
+# # read file
+# f = open("test.txt", "r")
+# print(f.read())
+# f.close()
 
-# append file
-f = open("test.txt", "a")
-f.write("\nnew line here")
-f.close()
+# # append file
+# f = open("test.txt", "a")
+# f.write("\nnew line here")
+# f.close()
+
+with open("test.txt","r",encoding="utf-8") as f:
+    # contents = f.readlines()
+    # print(lines)
+    # print(f.readline())
+    # print(f.readlines())
+    # for line in lines:
+    #     print(line)
+    # print(contents)
+    # [print(line.strip()) for line in contents]
+    for line in f:
+        print(line.strip())
