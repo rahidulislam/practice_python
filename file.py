971356dcb9a762cb4517341aff3d185721837273
Rahidul Islam <rahiseli@outlook.com>
2025-01-08 09:56:06 +0600

    I learn writing file

--- file.py
+++ file.py
@@ -13,7 +13,8 @@
 # f.write("\nnew line here")
 # f.close()
 
-with open("test.txt","r",encoding="utf-8") as f:
+# File reading
+# with open("test.txt","r",encoding="utf-8") as f:
     # contents = f.readlines()
     # print(lines)
     # print(f.readline())
@@ -22,5 +23,22 @@ with open("test.txt","r",encoding="utf-8") as f:
     #     print(line)
     # print(contents)
     # [print(line.strip()) for line in contents]
-    for line in f:
-        print(line.strip())
+    # for line in f:
+    #     print(line.strip())
+
+# file writing
+lines = ["Readme","How to write text files in python","Python is a programming language"]
+with open("test.txt","w") as f:
+    # f.write("I am writing to a file")
+    # for line in lines:
+    #     f.write(line)
+    #     f.write("\n")
+    f.writelines('\n'.join(lines))
+
+more_lines = ["","Java is a programming language", "C++ is a programming language"]
+with open("test.txt","a") as f:
+    f.writelines('\n'.join(more_lines))
+
+utf8_lines = ["",'আমার নাম রাহিদুল ইসলাম']
+with open("test.txt","a",encoding="utf-8") as f:
+    f.write("\n".join(utf8_lines))
